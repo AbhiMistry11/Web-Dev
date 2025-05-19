c37eb01afda317c3e70309bb9fee76bdd731456e
AbhiMistry11 <abhimistry07@gmail.com>
2025-05-19 23:57:31 +0530

    Day1

---
+++ javascript/SimonSays/app.js
@@ -0,0 +1,54 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red","yellow","green","purple"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", () => {
+  if (started == false) {
+    console.log("game is started");
+    started = true;
+    levelUp();
+  }
+});
+
+function gameFlash(btn){
+  btn.classList.add("flash");
+  setTimeout(function(){
+    btn.classList.remove("flash");
+  },1000);
+}
+
+function userFlash(btn){
+  btn.classList.add("userflash");
+  setTimeout(function(){
+    btn.classList.remove("userflash");
+  },1000);
+}
+
+function levelUp() {
+  level++;
+  h2.innerText = `Level ${level}`;
+
+  //random
+  let randIdx = Math.floor(Math.random()*3);
+  let randColor = btns[randIdx];
+  let randBtn = document.querySelector(`.${randColor}`);
+
+  gameFlash(randBtn);
+}
+
+function btnPress(){
+    console.log("btn pressed!!");
+    let btn = this;
+    userFlash(btn);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
