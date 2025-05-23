86e2ad6dc53c5c470671b23123bd4b285764994c
AbhiMistry11 <abhimistry07@gmail.com>
2025-05-23 21:18:31 +0530

    day 5

---
+++ BackEnd/nodejs/math.js
@@ -0,0 +1,13 @@
+const sum = (a,b)=> a+b;
+const multi = (a,b)=> a*b;
+const g = 9.8;
+const PI = 3.14;
+
+let obj = {
+    sum:sum,
+    multi:multi,
+    g:g,
+    PI:PI,
+};
+module.exports = obj;
+// module.exports = 123;
---
+++ BackEnd/nodejs/script.js
@@ -0,0 +1,19 @@
+// console.log(process.argv);
+
+// let args = process.argv;
+
+// for(let i=0;i<args.length;i++){
+//     console.log("hello to",args[i]);
+// };
+
+//module.exports
+
+// let math = require("./math");
+
+// console.log(math.sum(3,5));
+// console.log(math.multi(4,5));
+// console.log(math.PI);
+
+const info = require("../Fruits");
+
+console.log(info);
