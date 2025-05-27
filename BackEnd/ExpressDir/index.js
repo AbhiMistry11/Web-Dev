145c40c2701251e58129df3ddeca27cf4161031f
AbhiMistry11 <abhimistry07@gmail.com>
2025-05-27 22:30:11 +0530

    Day 9

--- BackEnd/ExpressDir/index.js
+++ BackEnd/ExpressDir/index.js
@@ -13,21 +13,35 @@ app.get("/", (req, res) => {
   res.send("you conatcted the root path");
 });
 
-app.get("/apple", (req, res) => {
-  res.send("you conatcted the apple path");
+// app.get("/:username/:id", (req, res) => {
+//   const { username, id } = req.params;
+//   console.log(req.params);
+//   const htmlStr = `<h1>Hello, My Username is ${username} and ID is ${id}.</h1>`
+//   res.send(htmlStr);
+// });
+
+app.get("/search",(req,res)=>{
+  const {q} = req.query;
+  console.log(req.query);
+  res.send(`The Search Result Of Query String : ${q}`);
 });
 
-app.get("/banana", (req, res) => {
-  res.send("you conatcted the banana path");
-});
+
+// app.get("/apple", (req, res) => {
+//   res.send("you conatcted the apple path");
+// });
+
+// app.get("/banana", (req, res) => {
+//   res.send("you conatcted the banana path");
+// });
 
 // app.get("*", (req, res) => {
 //   res.send("this page is not available!!");
 // });
 
-app.post("/",(req,res)=>{
-    res.send("you sent a post request");
-});
+// app.post("/",(req,res)=>{
+//     res.send("you sent a post request");
+// });
 
 // app.use((req,res)=>{
 //     console.log("request received");
