1b72e4c33ffe5a0c8949afa6f72a0b63dce3ff5b
AbhiMistry11 <abhimistry07@gmail.com>
2025-06-05 20:53:52 +0530

    day15

--- BackEnd/EJSDir/index.js
+++ BackEnd/EJSDir/index.js
@@ -6,9 +6,29 @@ let port = 8080;
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"/public/css")));
+app.use(express.static(path.join(__dirname,"/public/js")));
+
+
 
 app.get("/",(req,res)=>{
     res.render("home.ejs");
+});
+
+app.get("/ig/:username",(req,res)=>{
+  let {username} = req.params;
+  const instaData = require("./data.json");
+  const data = instaData[username];
+ if(data){
+  res.render("instagram.ejs", {data});
+ }else{
+  res.render("error.ejs");
+ };
+});
+
+app.get("/rolldice",(req,res)=>{
+  const diceValue = Math.floor(Math.random() * 6) + 1 ;
+    res.render("rolldice.ejs", {num : diceValue});
 });
 
 app.listen(port, () => {
